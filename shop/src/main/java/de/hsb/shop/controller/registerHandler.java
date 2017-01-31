e0030b4e3e995efefa94428bfe5f4b17c3af22b3
fiedler <fiedler@DESKTOP-BD.BALANCE.local>
2017-01-31 16:35:06 +0100

    mango

---
+++ shop/src/main/java/de/hsb/shop/controller/registerHandler.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hsb.shop.controller;
+
+import de.hsb.shop.model.Member;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author fiedler
+ */
+@ManagedBean
+@ViewScoped
+public class registerHandler implements Serializable {
+
+    @ManagedProperty(value = "#{loginHandler}")
+    private LoginHandler loginhandler;
+
+    @PersistenceContext
+    private EntityManager em;
+    @Resource
+    private UserTransaction utx;
+
+    private Member member;
+
+    @PostConstruct
+    public void init() {
+        if(loginhandler.isLogged()){
+            try {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("startpage.xhtml?faces-redirect=true");
+            } catch (IOException ex) {
+                Logger.getLogger(MemberHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } 
+        member = new Member();
+    }
+
+    public String save() {
+        try {
+            utx.begin();
+            member = em.merge(member);
+            em.persist(member);
+            utx.commit();
+        } catch (Exception ex) {
+            Logger.getLogger(registerHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "startpage.xhtml?faces-redirect=true";
+    }
+
+    public Member getMerkeMember() {
+        return member;
+    }
+
+    public void setMerkeMember(Member merkeMember) {
+        this.member = merkeMember;
+    }
+
+    public LoginHandler getLoginhandler() {
+        return loginhandler;
+    }
+
+    public void setLoginhandler(LoginHandler loginhandler) {
+        this.loginhandler = loginhandler;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+}
