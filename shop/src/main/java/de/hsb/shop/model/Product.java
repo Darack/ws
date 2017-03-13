e2279b92f1e7c78ed4fc1d37e3ce226640bda8b2
Tobi <Tobi@Tobi_LT.fritz.box>
2017-03-13 15:16:33 +0100

    Melone

---
+++ shop/src/main/java/de/hsb/shop/model/Product.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hsb.shop.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.validation.constraints.Size;
+
+@NamedQuery(name = "SelectProduct", query = "Select t from Product t")
+@Entity
+public class Product implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    
+    @Size(max = 255)
+    @Column(name = "name")
+    private String name;
+    
+    @Size(max = 255)
+    @Column(name = "description")
+    private String description;
+    
+    @Column(name = "price")
+    private Integer price;
+    
+    @JoinColumn(name = "productCategory", referencedColumnName = "id")
+    @ManyToOne
+    private ProductCategory productCategory;
+   
+    public Product(){}
+    
+    public Product(String name){
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
+    }
+}
---
+++ shop/src/main/java/de/hsb/shop/model/ProductCategory.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.hsb.shop.model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+
+@NamedQuery(name = "SelectProductCategory", query = "Select s from ProductCategory s")
+@Entity
+public class ProductCategory implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    
+    @Size(max = 255)
+    @Column(name = "name")
+    private String name;
+    
+    @OneToMany(mappedBy = "productCategory",cascade = CascadeType.ALL)
+    private List<Product> productList;
+    
+    public ProductCategory(){}
+    
+    public ProductCategory(String name){
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+}
